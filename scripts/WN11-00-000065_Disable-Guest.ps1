3bdeda8017dcc1cd2d293b1d79abe0d6c5ea6169
Mark Tuazon <marktuazon28@yahoo.com>
2026-03-21 12:50:16 -0400

    Create WN11-00-000065_Disable-Guest.ps1

---
+++ scripts/WN11-00-000065_Disable-Guest.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+    Disables Guest account.
+
+.NOTES
+    Author          : Mark Tuazon
+    LinkedIn        : linkedin.com/in/mark-t-83a52b145/
+    GitHub          : github.com/MarkCyberOps
+    Date Created    : 2026-03-21
+    Last Modified   : 2026-03-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-00-000065
+
+.TESTED ON
+    Date(s) Tested  : 2026-03-21
+    Tested By       : Mark Tuazon
+    Systems Tested  : Windows 11 Pro 25H2
+    PowerShell Ver. : 5.1
+
+.USAGE
+    Run with administrative privileges.
+#>
+
+Disable-LocalUser -Name "Guest"
